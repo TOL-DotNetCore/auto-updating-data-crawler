006106dce76ffe44e564c5b9a0db8083f128ba3c
Harry Trinh <40319325+harrytrinh123@users.noreply.github.com>
2022-02-28 23:30:01 +0700

    Add project files.

---
+++ auto-updating-data-crawler/Controllers/HangfireController.cs
@@ -0,0 +1,60 @@
+﻿using Hangfire;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace auto_updating_data_crawler.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class HangfireController : ControllerBase
+    {
+        [HttpPost]
+        [Route("[action]")]
+        public IActionResult Welcome()
+        {
+            var jobId = BackgroundJob.Enqueue(() => SendWelcomeEmail("Welcome to our app"));
+
+            return Ok($"Job ID: {jobId}. Welcome email sent to the user!");
+        }
+
+        [HttpPost]
+        [Route("[action]")]
+        public IActionResult Discount()
+        {
+            int timeInSeconds = 30;
+            var jobId = BackgroundJob.Schedule(() => SendWelcomeEmail("Welcome to our app"), TimeSpan.FromSeconds(timeInSeconds));
+
+            return Ok($"Job ID: {jobId}. Discount email will be sent in {timeInSeconds} seconds!");
+        }
+
+        [HttpPost]
+        [Route("[action]")]
+        public IActionResult DatabaseUpdate()
+        {
+            RecurringJob.AddOrUpdate(() => Console.WriteLine("Database updated"), Cron.Minutely);
+            return Ok("Database check job initiated!");
+        }
+
+        [HttpPost]
+        [Route("[action]")]
+        public IActionResult Confirm()
+        {
+            int timeInSeconds = 30;
+            var parentJobId = BackgroundJob.Schedule(() => Console.WriteLine("You asked to be unsubscribed!"), TimeSpan.FromSeconds(timeInSeconds));
+
+            BackgroundJob.ContinueJobWith(parentJobId, () => Console.WriteLine("You were unsubscribed!"));
+
+            return Ok("Confirmation job created!");
+        }
+
+
+        public void SendWelcomeEmail(string text)
+        {
+            Console.WriteLine(text);
+        }
+    }
+}
