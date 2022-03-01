48af67bdb574aa411ebce19ec4ea89dd317c4a64
Harry Trinh <40319325+harrytrinh123@users.noreply.github.com>
2022-03-01 20:52:30 +0700

    [Write] Add service crawler and write to excel

---
+++ auto-updating-data-crawler/Controllers/CrawlerController.cs
@@ -0,0 +1,27 @@
+﻿using auto_updating_data_crawler.Services;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace auto_updating_data_crawler.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CrawlerController : ControllerBase
+    {
+        private readonly ICrawlerService _crawlerService;
+        public CrawlerController(ICrawlerService crawlerService)
+        {
+            _crawlerService = crawlerService;
+        }
+        [HttpGet()]
+        public async Task<IActionResult> GetWeather()
+        {
+            var result = await _crawlerService.GetWeathers();
+            return Ok(result);
+        }
+    }
+}
---
+++ auto-updating-data-crawler/Models/weather.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace auto_updating_data_crawler.Models
+{
+    public class Weather
+    {
+        public DateTime Date { get; set; }
+        public string LargeTemp { get; set; }
+        public string SmallTemp { get; set; }
+        public string TextTemp { get; set; }
+    }
+}
